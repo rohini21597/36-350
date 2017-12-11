ed14b4775a0a7e50713ad8d562110063bd7bdaf2
rohini21597 <33258199+rohini21597@users.noreply.github.com>
2017-12-10 20:18:06 -0500

    Update hello_aliens.R

--- hello_aliens.R
+++ hello_aliens.R
@@ -1,5 +1,5 @@
-cat("Hello aliens, so I heard you intelligent extraterrestrials may someday become residents of these new Earth-like exoplanets that we will discover someday.
-discover one day. We are a bunch of human beings on Earth who would like to extend you a warm welcome onto our planet. Let me tell you a bit
+cat("Hello aliens, so I heard you intelligent extraterrestrials may someday become residents of these new Earth-like exoplanets that we will discover someday. 
+We are a bunch of human beings on Earth who would like to extend you a warm welcome onto our planet. Let me tell you a bit
 about myself. I am a student at Carnegie Mellon University in the country of the United States, in the state of Pennsylvania, in the city
 of Pittsburgh. I am studying Statistics, and thanks to my wonderful professor, I have been granted the opportunity to discover a bunch of
 new planets of which you all are soon-to-be residents")
